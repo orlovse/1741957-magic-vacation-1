4a1fcf3f855b714f754bc3e244d29d9290fd7239
orlovse <hajoff@gmail.com>
2021-02-16 15:36:22 +0200

    4. add class to body after loading

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,13 @@
 // modules
-import mobileHeight from './modules/mobile-height-adjust.js';
-import slider from './modules/slider.js';
-import menu from './modules/menu.js';
-import footer from './modules/footer.js';
-import chat from './modules/chat.js';
-import result from './modules/result.js';
-import form from './modules/form.js';
-import social from './modules/social.js';
-import FullPageScroll from './modules/full-page-scroll';
+import mobileHeight from "./modules/mobile-height-adjust.js";
+import slider from "./modules/slider.js";
+import menu from "./modules/menu.js";
+import footer from "./modules/footer.js";
+import chat from "./modules/chat.js";
+import result from "./modules/result.js";
+import form from "./modules/form.js";
+import social from "./modules/social.js";
+import FullPageScroll from "./modules/full-page-scroll";
 
 // init modules
 mobileHeight();
@@ -21,3 +21,8 @@ social();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+const body = document.querySelector("body");
+body.onload = () => {
+  body.classList.add("loaded");
+};
